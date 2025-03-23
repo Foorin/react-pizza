9d14e1e0ad84254dbb0ada67281c7b47a55b17c2
Marina Kislyakova <kvlmarina6@gmail.com>
2025-03-24 00:23:04 +0300

    5 lesson props useState render ulli

---
+++ src/components/Categories.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Categories() {
+  const [activeCategory, setActiveCategory] = React.useState(0);
+  const onClickCategory = (index) => {
+    setActiveCategory(index);
+  };
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((category, index) => (
+          <li onClick={() => onClickCategory(index)} className={activeCategory === index ? 'active' : ''}>
+            {category}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
